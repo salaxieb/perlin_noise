7e20db412669f33134bdb13e2bd9fb9e27c50dfb
salaxieb <ildar.salakhiev@mapbox.com>
2024-07-26 01:53:59 +0100

    added tests

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -1,6 +1,5 @@
 from perlin_noise import PerlinNoise
 import random
-import pytest
 
 
 def test_perlin_noise_works_in_1D():
@@ -47,3 +46,26 @@ def test_noise_value_doesnt_depend_on_space_size():
 def test_that_dist_in_0_1_range():
     noise = PerlinNoise(octaves = 20.496, seed = 1337)
     assert noise([125/1281, 1152/1281])
+
+
+def test_tiles_works():
+    noise = PerlinNoise(octaves = 1.496)
+    assert noise(0.5, tile_sizes=1) == noise(0.5)
+    assert noise([0.5, 0.5], tile_sizes=[43, 2345]) == noise([0.5, 0.5])
+    assert noise([1.5, 1.5], tile_sizes=[1, 1]) == noise([0.5, 0.5])
+    assert noise([2.5, 3.5], tile_sizes=[2, 3]) == noise([0.5, 0.5])
+
+
+def test_tiles_seamless():
+    p_noise = PerlinNoise(octaves = 2)
+    lim = 400
+    prev = 0
+    x_lim, y_lim = 5, 10
+    for i in range(0, lim, 100):
+        prev = p_noise([x_lim * i/lim, 0], tile_sizes=(2, 3))
+        for j in range(lim):
+            noise = p_noise([x_lim * i/lim, y_lim * j/lim], tile_sizes=(2, 3))
+            assert -1 < noise < 1
+            assert 0 <= abs(noise - prev) < 0.1
+            prev = noise
+
